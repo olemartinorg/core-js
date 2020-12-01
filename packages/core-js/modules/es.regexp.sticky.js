f0716ea644ec97463a507ae45ffdac1d60e5c284
Denis Pushkarev <zloirock@zloirock.ru>
2020-12-01 10:25:33 +0700

    drop `DESCRIPTORS` and `IE8_DOM_DEFINE` constants

--- packages/core-js/modules/es.regexp.sticky.js
+++ packages/core-js/modules/es.regexp.sticky.js
@@ -1,11 +1,10 @@
-var DESCRIPTORS = require('../internals/descriptors');
 var UNSUPPORTED_Y = require('../internals/regexp-sticky-helpers').UNSUPPORTED_Y;
 var defineProperty = require('../internals/object-define-property').f;
 var getInternalState = require('../internals/internal-state').get;
 var RegExpPrototype = RegExp.prototype;
 
 // `RegExp.prototype.sticky` getter
-if (DESCRIPTORS && UNSUPPORTED_Y) {
+if (UNSUPPORTED_Y) {
   defineProperty(RegExp.prototype, 'sticky', {
     configurable: true,
     get: function () {
