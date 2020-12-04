5a57c01648284cac0ddd5ac1cde4936ad0f2f3db
Denis Pushkarev <zloirock@zloirock.ru>
2020-12-04 14:58:40 +0700

    use keywords as property names without quotes

--- packages/core-js/modules/es.symbol.js
+++ packages/core-js/modules/es.symbol.js
@@ -213,7 +213,7 @@ $forEach(objectKeys(WellKnownSymbolsStore), function (name) {
 $({ target: SYMBOL, stat: true, forced: !NATIVE_SYMBOL }, {
   // `Symbol.for` method
   // https://tc39.github.io/ecma262/#sec-symbol.for
-  'for': function (key) {
+  for: function (key) {
     var string = String(key);
     if (has(StringToSymbolRegistry, string)) return StringToSymbolRegistry[string];
     var symbol = $Symbol(string);
