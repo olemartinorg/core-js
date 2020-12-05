7ad976e35ee238b14094d11219d4377122895e2f
Denis Pushkarev <zloirock@zloirock.ru>
2020-12-05 15:46:34 +0700

    drop `es.object.create`

--- packages/core-js/modules/es.object.create.js
+++
@@ -1,8 +0,0 @@
-var $ = require('../internals/export');
-var create = require('../internals/object-create');
-
-// `Object.create` method
-// https://tc39.github.io/ecma262/#sec-object.create
-$({ target: 'Object', stat: true }, {
-  create: create,
-});
